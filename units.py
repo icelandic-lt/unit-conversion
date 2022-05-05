c79e564d3579f19c7a8439d86039712f86c8a849
DavidErikMollberg <david.mollberg@tiro.is>
2022-05-05 12:51:30 +0000

    Adding breaking changes to numbers.py

---
+++ units.py
@@ -0,0 +1,66 @@
+verb = {
+    "f_et_kk_nf": "er",
+    "f_ft_kk_nf": "eru",
+    "f_at_af": "eru",
+    "f_et_hk_nf": "eru",
+    "f_ft_hk_nf": "eru",
+}
+distance_convert_to = {
+    "fet": "mörg",
+    "tommur": "margir",
+    "mílur": "margar",
+    "millimetrar": "margir",
+    "sentimetrar": "margir",
+    "metrar": "margir",
+    "kílómetrar": "margir",
+}
+
+distance_units = {
+    "millimetri": {
+        "f_et_kk_nf": "millimetri",
+        "f_ft_kk_nf": "millimetrar",
+        "f_at_af": "millimetrar",
+        "f_et_hk_nf": "millimetrar",
+        "f_ft_hk_nf": "millimetrar",
+    },
+    "sentimetri": {
+        "f_et_kk_nf": "sentimetri",
+        "f_ft_kk_nf": "sentimetrar",
+        "f_at_af": "sentimetrar",
+        "f_et_hk_nf": "metrar",
+        "f_ft_hk_nf": "sentimetrar",
+    },
+    "metri": {
+        "f_et_kk_nf": "metri",
+        "f_ft_kk_nf": "metrar",
+        "f_at_af": "metrar",
+        "f_et_hk_nf": "metrar",
+        "f_ft_hk_nf": "metrar",
+    },
+    "kílómetri": {
+        "f_et_kk_nf": "kílómetri",
+        "f_ft_kk_nf": "kílómetrar",
+        "f_at_af": "kílómetrar",
+        "f_et_hk_nf": "kílómetrar",
+        "f_ft_hk_nf": "kílómetrar",
+    },
+    "míla": {
+        "f_et_kvk_nf": "míla",
+        "f_ft_kk_nf": "mílur",
+        "f_at_af": "mílur",
+        "f_et_hk_nf": "mílur",
+        "f_ft_hk_nf": "mílur",
+    },
+    "tomma": {
+        "f_et_kvk_nf": "tomma",
+        "f_ft_kk_nf": "tommur",
+        "f_at_af": "tommur",
+        "f_et_hk_nf": "tommur",
+        "f_ft_hk_nf": "tommur",
+    },
+    "fet": {
+        "f_et_hk_nf": "fet",
+        "f_ft_hk_nf": "fet",
+        "f_at_af": "fet",
+    },
+}
