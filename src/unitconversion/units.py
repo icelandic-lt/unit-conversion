376b05a0781798f6d63d610ff7513a615d3ff3d3
DavidErikMollberg <david.mollberg@tiro.is>
2022-08-25 16:38:47 +0000

    major cleanup, setup as package, lots of bug fixes, added pytest

---
+++ src/unitconversion/units.py
@@ -0,0 +1,202 @@
+verb = {
+    "et_kk_nf": "er",
+    "et_hk_nf": "er",
+    "et_kvk_nf": "er",
+    "et_kk_þf": "er",
+    "ft_hk_nf": "eru",
+    "ft_kvk_nf": "eru",
+    "ft_kk_nf": "eru",
+    "at_af": "eru",
+}
+
+
+distance_convert_to = {
+    "fet": "mörg",
+    "tommur": "margar",
+    "mílur": "margar",
+    "millimetrar": "margir",
+    "sentimetrar": "margir",
+    "metrar": "margir",
+    "kílómetrar": "margir",
+}
+
+
+distance_convert_to_s3 = [
+    "fet",
+    "tommur",
+    "mílur",
+    "millimetra",
+    "sentimetra",
+    "metra",
+    "kílómetra",
+]
+
+
+distance_units_s1 = {
+    "millimetri": {
+        "et_kk_nf": "millimetri",
+        "ft_kk_nf": "millimetrar",
+        "at_af": "millimetrar",
+        # "et_kk_þf": "millimetri", # Gefur það sama og et_kk_nf
+    },
+    "sentimetri": {
+        "et_kk_nf": "sentimetri",
+        "ft_kk_nf": "sentimetrar",
+        "at_af": "sentimetrar",
+    },
+    "metri": {
+        "et_kk_nf": "metri",
+        "ft_kk_nf": "metrar",
+        "at_af": "metrar",
+    },
+    "kílómetri": {
+        "et_kk_nf": "kílómetri",
+        "ft_kk_nf": "kílómetrar",
+        "at_af": "kílómetrar",
+    },
+    "míla": {
+        "et_kvk_nf": "míla",
+        "ft_kvk_nf": "mílur",
+        "at_af": "mílur",
+    },
+    "tomma": {
+        "et_kvk_nf": "tomma",
+        "ft_kvk_nf": "tommur",
+        "at_af": "tommur",
+    },
+    "fet": {
+        "et_hk_nf": "fet",
+        "at_af": "fet",
+    },
+}
+distance_units_s2 = {
+    "millimetri": {
+        "et_kk_þgf": "millimetra",
+        "ft_kk_þgf": "millimetrum",
+        "at_af": "millimetrum",
+        "_": "millimetrar",
+    },
+    "sentimetri": {
+        "et_kk_þgf": "sentimetra",
+        "ft_kk_þgf": "sentimetrum",
+        "at_af": "sentimetrum",
+        "_": "sentimetrar",
+    },
+    "metri": {
+        "et_kk_þgf": "metra",
+        "ft_kk_þgf": "metrum",
+        "at_af": "metrum",
+        "_": "metrar",
+    },
+    "kílómetri": {
+        "et_kk_þgf": "kílómetra",
+        "ft_kk_þgf": "kílómetrum",
+        "at_af": "kílómetrum",
+        "_": "kílómetrar",
+    },
+    "míla": {
+        "et_kvk_þgf": "mílu",
+        "ft_kvk_þgf": "mílum",
+        "at_af": "mílum",
+        "_": "mílur",
+    },
+    "tomma": {
+        "et_kvk_þgf": "tommu",
+        "ft_kvk_þgf": "tommum",
+        "at_af": "tommum",
+        "_": "tommur",
+    },
+    "fet": {
+        "et_hk_þgf": "feti",
+        "ft_hk_þgf": "fetum",
+        "at_af": "fetum",
+        "_": "fet",
+    },
+}
+
+
+distance_units_s3 = {
+    "millimetri": {
+        "et_kk_þgf": "millimetra",
+        "ft_kk_þgf": "millimetrum",
+        "at_af": "millimetrum",
+        "_": "millimetrar",
+    },
+    "sentimetri": {
+        "et_kk_þgf": "sentimetra",
+        "ft_kk_þgf": "sentimetrum",
+        "at_af": "sentimetrum",
+        "_": "sentimetrar",
+    },
+    "metri": {
+        "et_kk_þgf": "metra",
+        "ft_kk_þgf": "metrum",
+        "at_af": "metrum",
+        "_": "metrar",
+    },
+    "kílómetri": {
+        "et_kk_þgf": "kílómetra",
+        "ft_kk_þgf": "kílómetrum",
+        "at_af": "kílómetrum",
+        "_": "kílómetrar",
+    },
+    "míla": {
+        "et_kvk_þgf": "mílu",
+        "ft_kvk_þgf": "mílum",
+        "at_af": "mílum",
+        "_": "mílur",
+    },
+    "tomma": {
+        "et_kvk_þgf": "tommu",
+        "ft_kvk_þgf": "tommum",
+        "at_af": "tommum",
+        "_": "tommur",
+    },
+    "fet": {
+        "et_hk_þgf": "feti",
+        "ft_hk_þgf": "fetum",
+        "at_af": "fetum",
+        "_": "fet",
+    },
+}
+
+volume_convert_to = {
+    "lítrar": "margir",
+    "millilítrar": "margir",
+    "desilítrar": "margir",
+    "grömm": "mörg",
+    "kílógrömm": "mörg",
+    "pund": "mörg",
+    "bollar": "margir",
+    "únsur": "margar",
+    "matskeiðar": "margar",
+    "pelar": "margir",
+}
+
+
+volume_unit_s1 = {
+    "lítri": {
+        "et_kk_nf": "lítri",
+        "ft_kk_nf": "lítrar",
+        "at_af": "lítrar",
+    },
+    "desilítri": {
+        "et_kk_nf": "desilítri",
+        "ft_kk_nf": "desilítrar",
+        "at_af": "desilítrar",
+    },
+    "millilítri": {
+        "et_kk_nf": "millilítri",
+        "ft_kk_nf": "millilítrar",
+        "at_af": "millilítrar",
+    },
+    "grömm": {
+        "et_hk_nf": "gramm",
+        "ft_hk_nf": "grömm",
+        "at_af": "grömm",
+    },
+    "peli": {
+        "et_kk_nf": "peli",
+        "ft_kk_nf": "pelar",
+    },
+}
---
+++ src/unitconversion/utils.py
@@ -0,0 +1,104 @@
+def get_field(s_obj, key):
+    """
+    Takes in a sentnce object and finds the values for the given key.
+    """
+    to_return = []
+    for s in s_obj:
+        if key in s:
+            to_return.append(s)
+    return to_return
+
+
+def load_numbers(file="numbers.tsv"):
+    """
+    Loads the tsv containing the number as a dict
+    """
+    to_return = []
+    with open(file) as f_in:
+        for line in f_in:
+            line = line.rstrip().split("\t")
+            to_return.append([line[0], line[1], line[2]])
+
+    return to_return
+
+
+from reynir_correct import check_single
+import os, logging
+
+
+def run_check_single(s):
+
+    return [s, check_single(s)]
+
+
+def test_output_with_GreynirCorrect(sentences, file):
+    from multiprocessing.dummy import Pool
+    import time
+
+    start_time = time.time()
+
+    # from concurrent import ProcessPoolExecutor
+
+    # git clone https://github.com/mideind/GreynirCorrect
+    # cd GreynirCorrect
+    ## [ Activate your virtualenv here if you have one ]
+    # pip install -e .
+
+    # list of cases that greynir correct dosen't handle correctly.
+    # We screen for these case to make the output more readable.
+
+    dont_check = [
+        "Ásláttarvilla: 'einni' -> 'einnig'",
+        "Orðið 'míla' gæti átt að vera 'mála'",
+    ]
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+
+    iterable, res = [x[2] for x in sentences], []
+    logging.info("Running greynir correct on the sentences")
+
+    with Pool(processes=5) as pool:
+        res = pool.map(run_check_single, iterable)
+
+    # res = [run_check_single(s) for s in iterable]
+    out_filename = os.path.join(os.path.dirname(file), "corrections.tsv")
+    with open(out_filename, "w") as f_out:
+        for s, sent in res:
+            for annotation in sent.annotations:
+                if annotation and not any([annotation.text in l for l in dont_check]):
+                    f_out.write("Original: " + s + "\n")
+                    f_out.write("Suggestion: " + str(sent) + "\n")
+                    f_out.write(annotation.text + "\n------\n")
+    t = time.time() - start_time
+    logging.info(f"Correction runtime {t:.2f} seconds\n\t\t\t{t/60:.2f} minutes")
+
+    logging.info(f"Corrections added to the file {out_filename}")
+
+
+def sanity_check(sentences):
+    """
+    Takes the lowest and highest number and checks if all numbers have been generated between that range
+    """
+
+    nums = [x[0] for x in sentences]
+    highest = max(nums)
+    lowest = min(nums)
+    missing = set()
+    for n in range(int(lowest), int(highest)):
+        n = str(n)
+        if n not in nums:
+            missing.add(n)
+    missing = sorted(missing)
+    print(f"Range {lowest} - {highest}")
+    if missing:
+        print("Missing:\n{}".format("\n".join(missing)))
+    else:
+        print("OK")
+
+
+def save_to_file(sentences, file="out.tsv"):
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+    logging.info(f"Saving to file {file}")
+    with open(file, "w") as f_out:
+        for line in sentences:
+
+            f_out.write(str(line[0]) + "\t" + line[1] + "\t" + line[2] + "\n")
